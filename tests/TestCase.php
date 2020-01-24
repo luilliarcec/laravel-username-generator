7ae34e799894950afcb3367f065c42eba1ad9ed4
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-01-24 16:13:48 -0500

    Riding the environment for tests

---
+++ tests/TestCase.php
@@ -0,0 +1,51 @@
+<?php
+
+
+namespace Luilliarcec\LaravelUsernameGenerator\Tests;
+
+use Luilliarcec\LaravelUsernameGenerator\UsernameGeneratorServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
+
+class TestCase extends Orchestra
+{
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withFactories(__DIR__ . '/../database/factories');
+    }
+
+    /**
+     * Get package providers.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return [UsernameGeneratorServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     *
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('laravel-username-generator.column', 'username');
+        $app['config']->set('database.default', 'testdb');
+        $app['config']->set('database.connections.testdb', [
+            'driver' => 'sqlite',
+            'database' => ':memory:'
+        ]);
+    }
+}
