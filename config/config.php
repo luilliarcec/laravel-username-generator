53cade1b200220f52b3d6c7caf7defe6a4bc0c93
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-01-24 16:12:43 -0500

    Update config file

--- config/config.php
+++ config/config.php
@@ -4,5 +4,34 @@
  * You can place your custom package configuration in here.
  */
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Type Case
+    |--------------------------------------------------------------------------
+    |
+    | The final result of the username will depend on the case selected.
+    | lower, upper or mixed
+    |
+    */
+    'case' => 'lower',
 
+    /*
+    |--------------------------------------------------------------------------
+    | Model Eloquent
+    |--------------------------------------------------------------------------
+    |
+    | The model who will have a username, to avoid repeated.
+    |
+    */
+    'model' => '\App\User',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Column Model Database
+    |--------------------------------------------------------------------------
+    |
+    | Name of the column where the username is stored.
+    |
+    */
+    'column' => 'username'
 ];
