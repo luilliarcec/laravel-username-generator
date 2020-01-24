075a6b2f041e44e83dcc962537d8aae93f6aee63
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-01-24 16:11:30 -0500

    Cleaning Service Provider

---
+++ src/UsernameGeneratorServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Luilliarcec\LaravelUsernameGenerator;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\ServiceProvider;
+use Luilliarcec\LaravelUsernameGenerator\Support\LaravelUsernameGenerator;
+
+class UsernameGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     */
+    public function boot()
+    {
+        /*
+         * Optional methods to load your package assets
+         */
+         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('laravel-username-generator.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-username-generator');
+
+        // Register the main class to use with the facade
+        $this->app->singleton('UsernameGenerator', function () {
+            return new LaravelUsernameGenerator;
+        });
+    }
+}
