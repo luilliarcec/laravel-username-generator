65e7dc46070ef60883d9495bbe0bfb7045bfaa47
Luis Andrés Arce C <luilliarcec@gmail.com>
2020-04-15 04:09:25 -0500

    Refactor code

---
+++ src/Contracts/UsernameDriverContract.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Luilliarcec\LaravelUsernameGenerator\Contracts;
+
+use Luilliarcec\LaravelUsernameGenerator\Exceptions\UsernameGeneratorException;
+
+interface UsernameDriverContract
+{
+    /**
+     * Create the username from the received parameters
+     *
+     * @param string $name Firstname or Email
+     * @param string $lastname Lastname
+     * @return string
+     * @throws UsernameGeneratorException
+     */
+    public function make(string $name, string $lastname = null): string;
+}
--- src/Support/UsernameGenerator.php
+++ src/Support/UsernameGenerator.php
@@ -2,8 +2,10 @@
 
 namespace Luilliarcec\LaravelUsernameGenerator\Support;
 
+use BadMethodCallException;
 use Error;
 use Illuminate\Support\Str;
+use Luilliarcec\LaravelUsernameGenerator\Contracts\UsernameDriverContract;
 use Luilliarcec\LaravelUsernameGenerator\Exceptions\UsernameGeneratorException;
 
 class UsernameGenerator
@@ -48,157 +50,19 @@ class UsernameGenerator
     }
 
     /**
-     * @param $name
-     * @param null $lastname
+     * Create the username from the received parameters
+     *
+     * @param string $name
+     * @param string|null $lastname
      * @return string
      * @throws UsernameGeneratorException
      */
-    public function make($name, $lastname = null)
+    public function make(string $name, string $lastname = null): string
     {
-        $this->validate($name, $lastname);
-
-        $username = $this->getDriver($name, $lastname);
-
+        $username = $this->getDriver()->make($name, $lastname);
         $username = $this->getCase($username);
-
         $prefix = $this->getPrefixUsername($username);
-
         return ($username . $prefix);
-    }
-
-    /**
-     * Generate user names by person names
-     *
-     * @param $name
-     * @param $lastname
-     * @return mixed|string
-     * @throws UsernameGeneratorException
-     */
-    protected function driverName($name, $lastname)
-    {
-        if (filter_var($name, FILTER_VALIDATE_EMAIL)) {
-            throw new UsernameGeneratorException("Use the email driver, to generate a username from the email.");
-        }
-
-        $words = $this->countName($name, $lastname);
-
-        switch ($words) {
-            case 1:
-            {
-                return $this->makeUsernameWithOneName($name);
-            }
-            case 2:
-            {
-                return $this->makeUsernameWithTwoName($name, $lastname);
-            }
-            case 3:
-            {
-                return $this->makeUsernameWithThreeName($name, $lastname);
-            }
-            case 4:
-            {
-                return $this->makeUsernameWithFullName($name, $lastname);
-            }
-            default:
-            {
-                throw new UsernameGeneratorException("Too many values have been provided to the username generator, the candidate words provided are: {$words}, the number of words supported is: 4.");
-            }
-        }
-    }
-
-    /**
-     * Generate user names by person emails
-     *
-     * @param $name
-     * @return mixed
-     * @throws UsernameGeneratorException
-     */
-    protected function driverEmail($name)
-    {
-        if (!filter_var($name, FILTER_VALIDATE_EMAIL)) {
-            throw new UsernameGeneratorException(
-                "The value provided does not have a valid email format."
-            );
-        }
-
-        $username = explode('@', $name);
-        return $username[0];
-    }
-
-    /**
-     * Make username with only one name
-     *
-     * @param $name
-     * @return mixed
-     */
-    protected function makeUsernameWithOneName($name)
-    {
-        return $name;
-    }
-
-    /**
-     * Make username with a two names or one surnames
-     *
-     * @param $name
-     * @param $lastname
-     * @return string
-     */
-    protected function makeUsernameWithTwoName($name, $lastname)
-    {
-        $name_array = explode(' ', $name);
-
-        $firstLetter = $this->getFirstLetterName($name_array);
-
-        return $lastname ? ($firstLetter . $lastname) : ($firstLetter . $name_array[1]);
-    }
-
-    /**
-     * Make username with a two names and one surnane or vice versa
-     *
-     * @param $name
-     * @param $lastname
-     * @return string
-     */
-    protected function makeUsernameWithThreeName($name, $lastname)
-    {
-        $name_array = explode(' ', $name);
-        $lastname_array = explode(' ', $lastname);
-
-        if (count($name_array) == 3) {
-            $lastname_array = array_slice($name_array, 1);
-        }
-
-        $firstLetter = $this->getFirstLetterName($name_array);
-        $firstLastname = $this->getFirstSurname($lastname_array);
-
-        $firstLetterSecondSurname = count($lastname_array) == 2 ?
-            $this->getFirstLetterSecondLastName($lastname_array) : '';
-
-        return ($firstLetter . $firstLastname . $firstLetterSecondSurname);
-    }
-
-    /**
-     * Make username with fullname
-     *
-     * @param $name
-     * @param $lastname
-     * @return string
-     */
-    protected function makeUsernameWithFullName($name, $lastname)
-    {
-        $name_array = explode(' ', $name);
-
-        $lastname_array = count($name_array) == 2 ?
-            explode(' ', $lastname) : array_slice($name_array, 2, 2);
-
-        $name_array = count($name_array) == 4 ?
-            array_slice($name_array, 0, 2) : $name_array;
-
-        $firstLetter = $this->getFirstLetterName($name_array);
-        $firstLastname = $this->getFirstSurname($lastname_array);
-        $firstLetterSecondSurname = $this->getFirstLetterSecondLastName($lastname_array);
-
-        return ($firstLetter . $firstLastname . $firstLetterSecondSurname);
     }
 
     /**
@@ -232,56 +96,6 @@ class UsernameGenerator
         }
 
         return $prefix;
-    }
-
-
-    /**
-     * Get the first letter of the first name
-     *
-     * @param array $firstname
-     * @return mixed
-     */
-    protected function getFirstLetterName(array $firstname)
-    {
-        $firstname_letters = str_split($firstname[0]);
-        return $firstname_letters[0];
-    }
-
-    /**
-     * Get the first last name
-     *
-     * @param array $lastname
-     * @return mixed
-     */
-    protected function getFirstSurname(array $lastname)
-    {
-        return $lastname[0];
-    }
-
-    /**
-     * Get the first letter of the second last name
-     *
-     * @param array $lastname
-     * @return string
-     */
-    protected function getFirstLetterSecondLastName(array $lastname)
-    {
-        $lastsurname_letters = str_split($lastname[1]);
-        return $lastsurname_letters[0];
-    }
-
-    /**
-     * Get the number of words that make up the name
-     *
-     * @param string $name
-     * @param string|null $lastname
-     * @return int
-     */
-    protected function countName($name, $lastname = null)
-    {
-        return $lastname ?
-            count(explode(' ', $name . ' ' . $lastname)) :
-            count(explode(' ', $name));
     }
 
     /**
@@ -319,36 +133,6 @@ class UsernameGenerator
     }
 
     /**
-     * Get the username according to the driver
-     *
-     * @param $name
-     * @param $lastname
-     * @return string
-     * @throws UsernameGeneratorException
-     */
-    protected function getDriver($name, $lastname)
-    {
-        switch ($this->driver) {
-            case 'name':
-            {
-                $username = $this->driverName($name, $lastname);
-                break;
-            }
-            case 'email':
-            {
-                $username = $this->driverEmail($name);
-                break;
-            }
-            default:
-            {
-                throw new UsernameGeneratorException("Driver not supported");
-            }
-        }
-
-        return $username;
-    }
-
-    /**
      * Get parsed by the case
      *
      * @param string $username
@@ -357,52 +141,27 @@ class UsernameGenerator
      */
     protected function getCase(string $username): string
     {
-        switch ($this->case) {
-            case 'lower':
-            {
-                $username = Str::lower($username);
-                break;
-            }
-            case 'upper':
-            {
-                $username = Str::upper($username);
-                break;
-            }
-            default:
-            {
-                throw new UsernameGeneratorException("Case not supported");
-            }
+        try {
+            return Str::{$this->case}($username);
+        } catch (BadMethodCallException $e) {
+            throw new UsernameGeneratorException('Case type not supported [' . strval($this->case) . ']: ' . $e->getMessage(), null, $e);
         }
-
-        return $username;
     }
 
     /**
-     * Valid that the general conditions of the package are met
+     * Get the driver instance
      *
-     * @param $name
-     * @param $lastname
+     * @return UsernameDriverContract
      * @throws UsernameGeneratorException
      */
-    protected function validate($name, $lastname)
+    public function getDriver(): UsernameDriverContract
     {
-        $countName = count(explode(' ', $name));
-        $countLastName = $lastname == null ? 0 : count(explode(' ', $lastname));
+        $driver = '\Luilliarcec\LaravelUsernameGenerator\Support\Drivers\\' . Str::studly($this->driver);
 
-        if ($name == null) {
-            throw new UsernameGeneratorException("The name cannot be null");
-        }
-
-        if ($countName == 4 && $countLastName > 0) {
-            throw new UsernameGeneratorException("The lastname parameter is not supported when the name has fullname (four words)");
-        }
-
-        if (($countName == 1 || $countName == 2) && $countLastName > 2) {
-            throw new UsernameGeneratorException("Only one or two lastname are supported");
-        }
-
-        if ($countName == 3 && $countLastName > 0) {
-            throw new UsernameGeneratorException("The lastname parameter is not supported when the name has three words");
+        try {
+            return new $driver;
+        } catch (Error $e) {
+            throw new UsernameGeneratorException('Driver type not supported [' . strval($this->driver) . ']: ' . $e->getMessage(), null, $e);
         }
     }
 }
