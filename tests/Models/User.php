2692e59f9c4a1989922c79e227f48118016abf5e
Luis Arce <luilliarcec@gmail.com>
2020-12-01 11:39:06 -0500

    Adding data default in create method instance of factories

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -35,4 +35,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Create a new Eloquent query builder for the model.
+     *
+     * @param  \Illuminate\Database\Query\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder|static
+     */
+    public function newEloquentBuilder($query)
+    {
+        return new UserQuery($query);
+    }
 }
