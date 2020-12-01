7192e705b8f527ee07801585616564e72eb83dd9
Luis Arce <luilliarcec@gmail.com>
2020-12-01 11:59:12 -0500

    Rename config to username-generator

--- src/UsernameGeneratorServiceProvider.php
+++ src/UsernameGeneratorServiceProvider.php
@@ -12,15 +12,9 @@ class UsernameGeneratorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-
-
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__.'/../config/config.php' => config_path('laravel-username-generator.php'),
+                __DIR__ . '/../config/config.php' => config_path('username-generator.php'),
             ], 'config');
         }
     }
@@ -31,7 +25,7 @@ class UsernameGeneratorServiceProvider extends ServiceProvider
     public function register()
     {
         // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'laravel-username-generator');
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'username-generator');
 
         // Register the main class to use with the facade
         $this->app->singleton('UsernameGenerator', function () {
