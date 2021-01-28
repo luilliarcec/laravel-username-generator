8f4210e181ca28e3c4879374bf1aecec80074362
Luis Arce <luilliarcec@gmail.com>
2021-01-28 15:45:52 -0500

    Username Case Exception Test

--- tests/Units/Exception/UsernameCaseExceptionTest.php
+++ tests/Units/Exception/UsernameCaseExceptionTest.php
@@ -2,54 +2,18 @@
 
 namespace Luilliarcec\LaravelUsernameGenerator\Tests\Units\Exception;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Luilliarcec\LaravelUsernameGenerator\Exceptions\UsernameGeneratorException;
 use Luilliarcec\LaravelUsernameGenerator\Facades\Username;
-use Luilliarcec\LaravelUsernameGenerator\Support\UsernameGenerator;
 use Luilliarcec\LaravelUsernameGenerator\Tests\TestCase;
 
 class UsernameCaseExceptionTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
-
-    /**
-     * @var UsernameGenerator
-     */
-    protected $usernameGenerator;
-
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->usernameGenerator = new UsernameGenerator();
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app['config']->set('username-generator.case', 'foo');
-    }
-
-    /**
-     * @test
-     * @throws UsernameGeneratorException
-     */
-    public function make_lower_username()
+    /** @test */
+    function an_exception_is_received_when_the_case_does_not_exist()
     {
         $this->expectException(UsernameGeneratorException::class);
         $this->expectExceptionMessage('Case type not supported [foo]: Method Illuminate\Support\Str::foo does not exist.');
 
-        $username = $this->usernameGenerator->make('Luis');
-        $this->assertEquals('luis', $username);
-
-        $username = Username::make('Luis');
-        $this->assertEquals('luis', $username);
+        Username::setCase('foo')->make('Luis');
     }
 }
