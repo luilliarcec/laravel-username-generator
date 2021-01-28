5752ce647915dceb08a5ec1e2b493655e25fc074
Luis Arce <luilliarcec@gmail.com>
2021-01-28 15:47:06 -0500

    Username Model Exception Test

--- tests/Units/Exception/UsernameModelExceptionTest.php
+++ tests/Units/Exception/UsernameModelExceptionTest.php
@@ -2,56 +2,20 @@
 
 namespace Luilliarcec\LaravelUsernameGenerator\Tests\Units\Exception;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Luilliarcec\LaravelUsernameGenerator\Exceptions\UsernameGeneratorException;
 use Luilliarcec\LaravelUsernameGenerator\Facades\Username;
-use Luilliarcec\LaravelUsernameGenerator\Support\UsernameGenerator;
 use Luilliarcec\LaravelUsernameGenerator\Tests\TestCase;
 
 class UsernameModelExceptionTest extends TestCase
 {
-    use RefreshDatabase, WithFaker;
-
-    /**
-     * @var UsernameGenerator
-     */
-    protected $usernameGenerator;
-
-    /**
-     * Setup the test environment.
-     *
-     * @return void
-     */
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->usernameGenerator = new UsernameGenerator();
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app['config']->set('username-generator.model', '\Luilliarcec\LaravelUsernameGenerator\User');
-    }
-
-    /**
-     * @test
-     * @throws UsernameGeneratorException
-     */
-    public function make_lower_username()
+    /** @test */
+    function an_exception_is_received_when_the_model_does_not_exist()
     {
         $this->expectException(UsernameGeneratorException::class);
         $this->expectExceptionMessage(
-            "Unable to instantiate the model [\Luilliarcec\LaravelUsernameGenerator\User]: Class '\Luilliarcec\LaravelUsernameGenerator\User' not found"
+            "Unable to instantiate the model [App\Foo]: Class 'App\Foo' not found"
         );
 
-        $username = $this->usernameGenerator->make('Luis');
-        $this->assertEquals('luis', $username);
-
-        $username = Username::make('Luis');
-        $this->assertEquals('luis', $username);
+        Username::setModel('App\Foo')->make('Luis');
     }
 }
