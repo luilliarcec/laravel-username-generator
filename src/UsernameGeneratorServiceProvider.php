a718b3fc5be3fd76eee6c7c8a8cdd7104c148059
Luis Arce <luilliarcec@gmail.com>
2021-01-28 16:07:14 -0500

    Removing the configuration file

--- src/UsernameGeneratorServiceProvider.php
+++ src/UsernameGeneratorServiceProvider.php
@@ -12,11 +12,7 @@ class UsernameGeneratorServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/config.php' => config_path('username-generator.php'),
-            ], 'config');
-        }
+        //
     }
 
     /**
@@ -24,10 +20,6 @@ class UsernameGeneratorServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Automatically apply the package configuration
-        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'username-generator');
-
-        // Register the main class to use with the facade
         $this->app->singleton('UsernameGenerator', function () {
             return new UsernameGenerator;
         });
