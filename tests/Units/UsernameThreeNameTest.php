85fcb93893709ee7181ebe2c60f9a66f7abd97a0
Luis Arce <luilliarcec@gmail.com>
2021-01-28 15:44:40 -0500

    Username ThreeName Test

---
+++ tests/Units/UsernameThreeNameTest.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Luilliarcec\LaravelUsernameGenerator\Tests\Units;
+
+use Luilliarcec\LaravelUsernameGenerator\Facades\Username;
+use Luilliarcec\LaravelUsernameGenerator\Tests\Models\User;
+use Luilliarcec\LaravelUsernameGenerator\Tests\TestCase;
+
+class UsernameThreeNameTest extends TestCase
+{
+    /** @test */
+    function make_username()
+    {
+        $this->assertEquals(
+            'larce',
+            Username::make('Luis Andrés', 'Arce')
+        );
+
+        $this->assertEquals(
+            'larcec',
+            Username::make('Luis', 'Arce Cárdenas')
+        );
+
+        $this->assertEquals(
+            'larcec',
+            Username::make('Luis Arce Cárdenas')
+        );
+    }
+
+    /** @test */
+    function make_username_with_similar_username_without_duplicate()
+    {
+        User::create(['username' => "larcea"]);
+
+        User::create(['username' => "larceca"]);
+
+        for ($i = 1; $i <= 2; $i++) {
+            User::create([
+                'username' => "larce{$i}"
+            ]);
+
+            User::create([
+                'username' => "larcec{$i}"
+            ]);
+        }
+
+        $this->assertEquals(
+            'larce',
+            Username::make('Luis Andrés', 'Arce')
+        );
+
+        $this->assertEquals(
+            'larcec',
+            Username::make('Luis', 'Arce Cárdenas')
+        );
+    }
+
+    /** @test */
+    function make_username_with_similars_username_and_duplicate()
+    {
+        User::create(['username' => "larce"]);
+
+        User::create(['username' => "larcec"]);
+
+        for ($i = 1; $i <= 2; $i++) {
+            User::create([
+                'username' => "larce{$i}"
+            ]);
+
+            User::create([
+                'username' => "larcec{$i}"
+            ]);
+        }
+
+        $this->assertEquals(
+            'larce3',
+            Username::make('Luis Andrés', 'Arce')
+        );
+
+        $this->assertEquals(
+            'larcec3',
+            Username::make('Luis', 'Arce Cárdenas')
+        );
+    }
+}
